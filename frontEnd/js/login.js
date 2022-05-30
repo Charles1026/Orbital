8a04e1373838a8f9c028da6b927dcbd27ab999f1
Charles1026 <xiaozhehao@gmail.com>
2022-05-30 11:26:32 +0800

    Front End Login Page Added and Database Connector Added

--- frontEnd/js/login.js
+++ frontEnd/js/login.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
-  $("#submitBtn").click(function(){        
-      $("#myForm").submit(); // Submit the form
+  $("#submitBut").click( function () {
+    // console.log("Hi");
+    var uname = $("#uname").val();
+    var pass = $("#pwd").val();
+    // console.log(uname)
+    // var str = "Hello " + uname + " with pword " + pass;
+    // alert(str);
+
+    $.post("https://httpbin.org/post  ", {user: uname, pwrd: pass}, 
+        function(data) {
+          alert(data.headers.Host);
+        }
+    );
   });
 });
--- src/DatabaseConnector.java
+++ src/DatabaseConnector.java
@@ -1,4 +1,4 @@
-package orbital;
+// package orbital;
 
 import java.sql.*;
 
@@ -6,20 +6,95 @@ public class DatabaseConnector {
   
   private Connection connection;
 
+  private String loginTable = "users";
+
   public DatabaseConnector() {
-    Class.forName("com.mysql.jdbc.Driver");
+    try {
+      Class.forName("com.mysql.cj.jdbc.Driver");  
+    } catch (ClassNotFoundException e) {
+      System.out.println(e.getMessage());
+    }
   }
 
-  public boolean connectToSQL(String url, String username, String password) {
-    this.connection = DriverManager.getConnection(url, username, password);
+  public boolean connectToDB(String url, String username, String password) {
+    try {
+      this.connection = DriverManager.getConnection(url, username, password);
+      return true;
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+      return false;
+    }
+  }
 
-    return true;
+  private boolean userRegistered(String user) {
+    String query = "SELECT uname FROM " + this.loginTable + " login WHERE uname = ?;";
+    try {
+      PreparedStatement statement = connection.prepareStatement(query);
+      statement.setString(1, user);
+      ResultSet results = statement.executeQuery();
+
+      results.next();
+
+      statement.close();
+      return true;
+
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+      return false;
+    }
+  }
+
+  public boolean verifyPass(String user, String pass) {
+    if (!userRegistered(user)) {
+      System.out.println("User Does not Exist.");
+      return false;
+    }
+    String query = "SELECT pword FROM " + this.loginTable + " WHERE uname=?";
+    try {
+      PreparedStatement statement = connection.prepareStatement(query);
+      statement.setString(1, user);
+      ResultSet results = statement.executeQuery();
+
+      results.next();
+
+      boolean verified = results.getString("pword").equals(pass);
+
+      statement.close();
+      return verified;
+
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+      return false;
+    }
+  }
+
+  public boolean createUser(String user, String pass) {
+    if (userRegistered(user)) {
+      System.out.println("User Already Registered.");
+      return false;
+    }
+    String query = "INSERT INTO " + this.loginTable + "(uname, pword) VALUES (?,?)";
+    try {
+      PreparedStatement statement = connection.prepareStatement(query);
+      statement.setString(1, user);
+      statement.setString(2, pass);
+
+      boolean created = statement.executeUpdate() == 0;
+
+      statement.close();
+      return created;
+
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+      return false;
+    }
   }
 
   
   public static void main(String[] args) {
     DatabaseConnector connector = new DatabaseConnector();
-
-    connector.connectToSQL("jdbc:mysql://localhost:3306/test","test","12345678");
+    connector.connectToDB("jdbc:mysql://localhost:3306/test","test","12345678");
+    connector.createUser("test", "1234");
+    System.out.println(connector.verifyPass("test", "1234"));
   }
 }
