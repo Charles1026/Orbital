efc73c0140ef14b375fe9bfeaf3d00baeca5c626
Charles1026 <xiaozhehao@gmail.com>
2022-06-08 10:56:26 +0800

    Created Framework for HTTP Server and added README

--- src/http_server/Server.java
+++ src/http_server/Server.java
@@ -12,37 +12,28 @@ import com.sun.net.httpserver.HttpServer;
 
 public class Server {
 
-    private String url;
-    private int port;
-    private DatabaseConnector db;
-
-    public Server(String url, int port, DatabaseConnector db) {
-        this.url = url;
-        this.port = port;
-        this.db = db;
-    }
-
-    public boolean startServer() {
+    public static boolean startServer(String url, int port) {
         try {
             HttpServer server = HttpServer.create(new InetSocketAddress(url, port), 0);
             server.createContext("/", new HomeHandler()); // Assigns a handler to the base website path
             server.createContext("/login", new LoginHandler()); // Assigns a handler to the base website path
+            server.createContext("/register", new RegisterHandler()); // Assigns a handler to the base website path
             server.setExecutor(null); // Uses the default executor for now
             server.start();
-
-            this.db.connectToDB("test", "users", "test","12345678");
-
             return true;
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
             return false;
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        DatabaseConnector db = new LoginConnector();
-        Server server = new Server("localhost", 80, db);
-        System.out.println("Hello");
-        server.startServer();
+    public static void main(String[] args) {
+        if (startServer("localhost", 8080)) {
+            System.out.println("Server Started");
+        } else {
+            System.out.println("Error Server not Started");
+            return;
+        }
+        System.out.println("Server Closed");
     }
 }
