0fbef4373d2b256810b99d8e5e7ac325293c8f58
Ryan Tan <ryantancw00@gmail.com>
2022-06-29 01:03:48 +0800

    deleting some json files, node_module and editing database for my MySQL db

--- frontEnd/database.js
+++ frontEnd/database.js
@@ -13,15 +13,10 @@ const connection = mysql.createPool({
 module.exports.connection = connection;
 
 module.exports.userTable = "users"; // Table for Users
-<<<<<<< HEAD
-module.exports.userUName = "username"; // Column for Username
-module.exports.userPWord = "password"; // Column for Password
-=======
-module.exports.userUName = "uname"; // Table for Users
-module.exports.userPWord = "pword"; // Table for Users
+module.exports.userUName = "username"; // Table for Users
+module.exports.userPWord = "password"; // Table for Users
 module.exports.userEmail = "email"; // Table for Users
 module.exports.userPos = "pos"; // Table for Users
 module.exports.userExp = "exp"; // Table for Users
->>>>>>> origin/main
 module.exports.courtTable = "courts"
 // module.exports.
