b55ec93d77127f3e9dc3f3d78d7166d012124ad3
Ryan Tan <ryantancw00@gmail.com>
2022-06-29 00:44:28 +0800

    editing stuff to my own setup

--- frontEnd/handlers/loginHandler.js
+++ frontEnd/handlers/loginHandler.js
@@ -16,7 +16,7 @@ const loginHandler = (req, res) => {
   const { uname, pswd } = req.body;
   console.log(req.body);
   database.connection.query(
-    `SELECT * FROM ${database.userTable} WHERE uname = '${uname}'`, 
+    `SELECT * FROM ${database.userTable} WHERE ${database.userUname} = '${uname}'`, 
     (err, results, fields) => {
         if (err) {
           console.log("Login Unsuccessful:" + err.stack);
